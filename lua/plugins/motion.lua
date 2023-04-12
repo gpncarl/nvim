dba4c0de0fc35cbe1892a0668ad3137a6bfbfc31
gpncarl <gpncarl@gmail.com>
2023-04-12 10:43:18 +0800

    replace CamelCaseMotion with vim-wordmotion

--- lua/plugins/motion.lua
+++ lua/plugins/motion.lua
@@ -94,19 +94,14 @@ return {
             l.add_default_mappings()
         end
     },
+    { "ggandor/leap-spooky.nvim", lazy = true, config = true },
+    { "ggandor/flit.nvim",        lazy = true, config = true },
     {
-        "ggandor/leap-spooky.nvim",
-        lazy = true,
-        config = true
-    },
-    { "ggandor/flit.nvim", lazy = true, config = true },
-    {
-        "bkad/CamelCaseMotion",
+        "chaoren/vim-wordmotion",
         event = "ModeChanged *:no",
         keys = { "<leader>w", "<leader>b", "<leader>e", "<leader>ge" },
         init = function()
-            vim.g.camelcasemotion_key = "<leader>"
-            return nil
+            vim.g.wordmotion_prefix = "<leader>"
         end
     },
 }
