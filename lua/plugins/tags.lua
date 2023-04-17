67c820577143000cd81eb2490d4933bc339ed3c2
gpncarl <gpncarl@gmail.com>
2023-04-17 11:11:40 +0800

    disable gutentags default

--- lua/plugins/tags.lua
+++ lua/plugins/tags.lua
@@ -2,6 +2,8 @@ return {
     {
         "ludovicchabant/vim-gutentags",
         init = function()
+            vim.g.gutentags_enabled = false
+            vim.g.gutentags_define_advanced_commands = true
             vim.g.gutentags_modules = { "ctags" }
             vim.g.gutentags_ctags_exclude = { ".ccls-cache", ".cache", ".clangd", ".vscode" }
         end
