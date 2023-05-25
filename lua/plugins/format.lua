f413b5f109e886e98e426eb53ece07e5184e53f3
gpncarl <gpncarl@gmail.com>
2023-05-25 14:25:38 +0800

    add treesj

--- lua/plugins/format.lua
+++ lua/plugins/format.lua
@@ -1,3 +1,15 @@
 return {
     { "godlygeek/tabular", lazy = true },
+    {
+        "Wansmer/treesj",
+        keys = { "<leader>J" },
+        cmd = { "TSJToggle", "TSJSplit", "TSJJoin" },
+        dependencies = { "nvim-treesitter/nvim-treesitter" },
+        opts = {
+            use_default_keymaps = true,
+        },
+        config = function()
+            vim.keymap.set('n', '<leader>J', require('treesj').toggle)
+        end
+    }
 }
