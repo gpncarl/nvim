d6a8b957f03eda0231cb6c6f084bd5ac6bf097c1
gpncarl <gpncarl@gmail.com>
2023-05-30 23:19:57 +0800

    Revert "disable q in alpha dashboard"
    This reverts commit a73d2491a73ea7a9e4b11a55d6b8e8d0c98b83a4.

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -9,7 +9,6 @@ return {
             local alpha = require("alpha")
             local startify = require("alpha.themes.startify")
             startify.nvim_web_devicons.enabled = true
-            startify.section.bottom_buttons.val = {}
             return alpha.setup(startify.config)
         end
     },
