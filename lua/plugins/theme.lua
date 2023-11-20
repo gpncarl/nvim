c0fac2c2f4d853ae4c31110f4b2ba098a71418ed
gpncarl <gpncarl@gmail.com>
2023-11-20 13:03:55 +0800

    add theme catppuccin

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -38,5 +38,12 @@ return {
         lazy = true,
         priority = 1000,
         opts = {},
+    },
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        lazy = true,
+        priority = 1000,
+        opts = {},
     }
 }
