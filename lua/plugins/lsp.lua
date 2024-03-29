1cea4213c7353a1312a25fba50eceedbd53a0c7b
gpncarl <gpncarl@gmail.com>
2024-03-29 21:24:14 +0800

    replace all outline-plugin with trouble.nvim:v3

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -92,13 +92,41 @@ end
 return {
     {
         "folke/trouble.nvim",
-        cmd = { "Trouble", "TroubleToggle", "TroubleClose", "TroubleRefresh" },
-        keys = { "<leader>t" },
-        config = function()
-            local t = require("trouble")
-            t.setup({ icons = true })
-            return vim.keymap.set("n", "<leader>t", "<Cmd>TroubleToggle<CR>", { desc = "toggle troube" })
-        end
+        branch = "dev",
+        cmd = { "Trouble" },
+        keys = {
+            {
+                "<leader>xx",
+                "<cmd>Trouble diagnostics toggle<cr>",
+                desc = "Diagnostics (Trouble)",
+            },
+            {
+                "<leader>xX",
+                "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+                desc = "Buffer Diagnostics (Trouble)",
+            },
+            {
+                "<leader>cs",
+                "<cmd>Trouble symbols toggle focus=false<cr>",
+                desc = "Symbols (Trouble)",
+            },
+            {
+                "<leader>cl",
+                "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+                desc = "LSP Definitions / references / ... (Trouble)",
+            },
+            {
+                "<leader>xL",
+                "<cmd>Trouble loclist toggle<cr>",
+                desc = "Location List (Trouble)",
+            },
+            {
+                "<leader>xQ",
+                "<cmd>Trouble qflist toggle<cr>",
+                desc = "Quickfix List (Trouble)",
+            },
+        },
+        opts = {}
     },
     {
         "neovim/nvim-lspconfig",
