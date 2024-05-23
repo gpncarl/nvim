2102f46fedc715b06a027e849608d527fe6de2e4
gpncarl <gpncarl@gmail.com>
2024-05-23 14:24:39 +0800

    add autocmds.lua

--- lua/setup.lua
+++ lua/setup.lua
@@ -1,3 +1,5 @@
+require "autocmds"
+
 local function switchHeader()
     local alt_exts = {
         c = { "h" },
@@ -46,59 +48,6 @@ vim.diagnostic.config {
 }
 
 vim.keymap.set("t", "<c-w>", "<c-\\><c-n><c-w>")
-
-local quickfix = vim.api.nvim_create_augroup('quickfix', { clear = true })
-vim.api.nvim_create_autocmd("QuickFixCmdPre", {
-    group = quickfix,
-    command = "packadd cfilter"
-})
-
-HasStdin = false
-local stdin = vim.api.nvim_create_augroup('stdin', { clear = true })
-vim.api.nvim_create_autocmd("StdinReadPre", {
-    group = stdin,
-    callback = function()
-        HasStdin = true
-    end
-})
-
-local config = require "config"
-local enter = vim.api.nvim_create_augroup('enter', { clear = true })
-vim.api.nvim_create_autocmd("UIEnter", {
-    group = enter,
-    callback = function()
-        if HasStdin then
-            return
-        end
-
-        local argc = vim.fn.argc()
-        if argc == 0 then
-            if config.dashboard == "alpha" then
-                require "alpha".start(true)
-            elseif config.dashboard == "mini.starter" then
-                require "mini.starter".open()
-            elseif config.dashboard == "dashboard" then
-                require "dashboard":instance()
-            end
-        end
-
-        if vim.fn.isdirectory(vim.fn.argv(0)) == 1 then
-            vim.api.nvim_exec_autocmds("User", { pattern = "OpenDirectory" })
-        end
-    end
-})
-
-local theme = vim.api.nvim_create_augroup('theme', { clear = true })
-vim.api.nvim_create_autocmd("ColorScheme", {
-    group = theme,
-    callback = function()
-        local fg_add = "guifg=" .. (vim.fn.synIDattr(vim.fn.synIDtrans(vim.fn.hlID("DiffAdd")), "bg", "gui") or "fg")
-        vim.cmd.highlight({ "diffAdded", fg_add })
-
-        local fg_change = "guifg=" .. vim.fn.synIDattr(vim.fn.synIDtrans(vim.fn.hlID("DiffChange")), "bg", "gui")
-        vim.cmd.highlight({ "diffChanged", fg_change })
-    end,
-})
 
 vim.g.netrw_list_hide = "\\(^\\|\\s\\s\\)\\zs\\.\\S\\+"
 vim.g.netrw_winsize = 20
