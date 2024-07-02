e954e771e5cf3f6abf4d02feafca81c248430804
gpncarl <gpncarl@gmail.com>
2024-07-02 09:47:39 +0800

    fix(build): telescope-fzf-native

--- lua/plugins/finder.lua
+++ lua/plugins/finder.lua
@@ -82,8 +82,7 @@ return {
         dependencies = {
             {
                 "nvim-telescope/telescope-fzf-native.nvim",
-                build =
-                "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build"
+                build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release --fresh && cmake --build build --config Release"
             },
         },
         config = telescope_config
