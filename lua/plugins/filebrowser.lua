da9973644a02e023f6cd24fd84dae840cf07d571
gpncarl <gpncarl@gmail.com>
2024-07-03 16:05:54 +0800

    fix neo-tree

--- lua/plugins/filebrowser.lua
+++ lua/plugins/filebrowser.lua
@@ -19,7 +19,7 @@ return {
             { "<space>ft", "<cmd>Neotree toggle<cr>",         desc = "Neotree toggle" },
             { "<space>fb", "<cmd>Neotree buffers toggle<cr>", desc = "Neotree buffers toggle" } },
         opts = {
-            buffers = { follow_current_file = true, group_empty_dirs = true, show_unloaded = true },
+            buffers = { follow_current_file = { enabled = true }, group_empty_dirs = true, show_unloaded = true },
             filesystem = { hijack_netrw_behavior = "disabled" }
         }
     }
