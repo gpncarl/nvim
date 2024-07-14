881be504d47d6dd3cee441595c087c6002218976
gpncarl <gpncarl@gmail.com>
2024-07-15 01:40:00 +0800

    correct the order of dependency for lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,31 +1,30 @@
-local function lsp_setup()
-    local lspconfig = require("lspconfig")
-    local function on_attach(client, bufnr)
-        vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = bufnr, desc = "goto define" })
-        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = bufnr, desc = "goto declaration" })
-        vim.keymap.set("n", "gr", vim.lsp.buf.references, { buffer = bufnr, desc = "goto ref" })
-        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { buffer = bufnr, desc = "goto implementation" })
-        vim.keymap.set("n", "ga", vim.lsp.buf.code_action, { buffer = bufnr, desc = "code action" })
-        vim.keymap.set({ "n", "v" }, "<space>fm", vim.lsp.buf.format, { buffer = bufnr, desc = "format" })
-        vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, { buffer = bufnr, desc = "rename" })
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = bufnr, desc = "code hover" })
-        vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, { buffer = bufnr, desc = "code signature" })
+local on_attach = function(client, bufnr)
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = bufnr, desc = "goto define" })
+    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = bufnr, desc = "goto declaration" })
+    vim.keymap.set("n", "gr", vim.lsp.buf.references, { buffer = bufnr, desc = "goto ref" })
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { buffer = bufnr, desc = "goto implementation" })
+    vim.keymap.set("n", "ga", vim.lsp.buf.code_action, { buffer = bufnr, desc = "code action" })
+    vim.keymap.set({ "n", "v" }, "<space>fm", vim.lsp.buf.format, { buffer = bufnr, desc = "format" })
+    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, { buffer = bufnr, desc = "rename" })
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = bufnr, desc = "code hover" })
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, { buffer = bufnr, desc = "code signature" })
 
-        if client.supports_method("textDocument/inlayHint") then
-            vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
-        end
-
-        if client.supports_method("textDocument/codeLens") then
-            local refresh = function() vim.lsp.codelens.refresh({ bufnr = bufnr }) end
-            refresh()
-            vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "InsertLeave" }, {
-                buffer = bufnr,
-                callback = refresh,
-            })
-        end
+    if client.supports_method("textDocument/inlayHint") then
+        vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
     end
 
-    local default_opts = {
+    if client.supports_method("textDocument/codeLens") then
+        local refresh = function() vim.lsp.codelens.refresh({ bufnr = bufnr }) end
+        refresh()
+        vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "InsertLeave" }, {
+            buffer = bufnr,
+            callback = refresh,
+        })
+    end
+end
+
+local get_default_opts = function()
+    return {
         inlay_hints = { enabled = true, },
         codelens = { enabled = true, },
         document_highlight = { enabled = true, },
@@ -36,33 +35,6 @@ local function lsp_setup()
         capabilities = require('cmp_nvim_lsp').default_capabilities(),
         on_attach = on_attach,
     }
-
-    require("mason-lspconfig").setup({
-        ensure_installed = { "clangd", "lua_ls" },
-        handlers = {
-            function(server_name) lspconfig[server_name].setup(default_opts) end,
-            clangd = function()
-                lspconfig.clangd.setup(vim.tbl_extend("force", default_opts, {
-                    on_attach = function(client, bufnr)
-                        on_attach(client, bufnr)
-                        require("clangd_extensions").setup()
-                        vim.keymap.set("n", "<space>sh", "<cmd>ClangdSwitchSourceHeader<cr>",
-                            { buffer = bufnr, desc = "switch header" })
-                    end,
-                }))
-            end,
-            lua_ls = function()
-                lspconfig.lua_ls.setup(vim.tbl_extend("force", default_opts, {
-                    settings = {
-                        Lua = {
-                            diagnostics = { globals = { "vim" } },
-                            codeLens = { enable = true, }
-                        }
-                    }
-                }))
-            end
-        }
-    })
 end
 
 return {
@@ -105,13 +77,8 @@ return {
     },
     {
         "neovim/nvim-lspconfig",
-        event = { "BufReadPre", "BufNewFile" },
-        dependencies = {
-            "folke/neoconf.nvim",
-            "williamboman/mason.nvim",
-            "williamboman/mason-lspconfig.nvim",
-        },
-        config = lsp_setup
+        lazy = true,
+        dependencies = { "folke/neoconf.nvim", },
     },
     {
         "williamboman/mason.nvim",
@@ -122,8 +89,40 @@ return {
     {
         "williamboman/mason-lspconfig.nvim",
         event = { "BufReadPre", "BufNewFile" },
-        dependencies = { "williamboman/mason.nvim", },
-        opts = {}
+        dependencies = {
+            "williamboman/mason.nvim",
+            "neovim/nvim-lspconfig",
+        },
+        opts = function()
+            local default_opts = get_default_opts()
+            local lspconfig = require("lspconfig")
+            return {
+                ensure_installed = { "clangd", "lua_ls" },
+                handlers = {
+                    function(server_name) lspconfig[server_name].setup(default_opts) end,
+                    clangd = function()
+                        lspconfig.clangd.setup(vim.tbl_extend("force", default_opts, {
+                            on_attach = function(client, bufnr)
+                                on_attach(client, bufnr)
+                                require("clangd_extensions").setup()
+                                vim.keymap.set("n", "<space>sh", "<cmd>ClangdSwitchSourceHeader<cr>",
+                                    { buffer = bufnr, desc = "switch header" })
+                            end,
+                        }))
+                    end,
+                    lua_ls = function()
+                        lspconfig.lua_ls.setup(vim.tbl_extend("force", default_opts, {
+                            settings = {
+                                Lua = {
+                                    diagnostics = { globals = { "vim" } },
+                                    codeLens = { enable = true, }
+                                }
+                            }
+                        }))
+                    end
+                }
+            }
+        end
     },
     {
         "jay-babu/mason-null-ls.nvim",
@@ -132,10 +131,14 @@ return {
             "williamboman/mason.nvim",
             "nvimtools/none-ls.nvim",
         },
-        opts = {
-            ensure_installed = {},
-            handlers = {},
-        }
+        opts = function()
+            return {
+                ensure_installed = {},
+                handlers = {
+                    nil, -- use default handler
+                },
+            }
+        end
     },
     {
         "nvimtools/none-ls.nvim",
