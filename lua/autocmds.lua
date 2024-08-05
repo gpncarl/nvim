6eb7c6b92802a738c6c734cae6e9165be5c4567b
gpncarl <gpncarl@gmail.com>
2024-08-05 16:48:12 +0800

    fix(terminal): fill terminal margin with the same color of background

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -155,3 +155,17 @@ vim.api.nvim_create_autocmd("UIEnter", {
     end
   end
 })
+
+vim.api.nvim_create_autocmd({ "UIEnter", "ColorScheme" }, {
+  callback = function()
+    local normal = vim.api.nvim_get_hl(0, { name = "Normal" })
+    if not normal.bg then return end
+    io.write(string.format("\027Ptmux;\027\027]11;#%06x\007\027\\", normal.bg))
+  end,
+})
+
+vim.api.nvim_create_autocmd("UILeave", {
+  callback = function()
+    io.write("\027Ptmux;\027\027]111;\007\027\\")
+  end,
+})
