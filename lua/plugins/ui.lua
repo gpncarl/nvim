bb92db7033936c630c48dec9aaca3f2b05a2185b
gpncarl <gpncarl@gmail.com>
2024-08-13 00:45:09 +0800

    refactor(icons): replace nvim-web-devicons with mini.icons again

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,8 +1,21 @@
 local config = require("config")
 return {
   { "nvim-lua/popup.nvim",         lazy = true },
-  { "nvim-tree/nvim-web-devicons", lazy = true },
   { "MunifTanjim/nui.nvim",        lazy = true },
+  {
+    "echasnovski/mini.icons",
+    lazy = true,
+    specs = {
+      { "nvim-tree/nvim-web-devicons", enabled = false, optional = true },
+    },
+    init = function()
+      package.preload["nvim-web-devicons"] = function()
+        require("mini.icons").mock_nvim_web_devicons()
+        return package.loaded["nvim-web-devicons"]
+      end
+    end,
+    opts = {},
+  },
   {
     "rcarriga/nvim-notify",
     enabled = config.popup_notify,
