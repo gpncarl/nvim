1ff2940b9e8e94f490626ba2bfc7ad6427656027
gpncarl <gpncarl@gmail.com>
2024-10-18 11:06:01 +0800

    feat: remove gitgraph

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -23,10 +23,6 @@ return {
       "Gvdiffsplit",
       "Ghdiffsplit",
     },
-    keys = {
-      { "<leader>gb", "<cmd>Git blame<cr>", desc = "git blame" },
-      { "<leader>gD", "<cmd>Gdiffsplit<cr>", desc = "git diff split" }
-    },
   },
   {
     "sindrets/diffview.nvim",
@@ -58,39 +54,5 @@ return {
     keys = { { "<leader>gg", "<cmd>Neogit<cr>", desc = "Open Neogit" } },
     dependencies = { "sindrets/diffview.nvim" },
     opts = {}
-  },
-  {
-    "isakbm/gitgraph.nvim",
-    cmd = "GitGraph",
-    keys = { { "<leader>gG", "<cmd>GitGraph<cr>", desc = "Open GitGraph" } },
-    opts = {
-      symbols = {
-        -- commit = '○',
-        -- commit_end = '○',
-        -- merge_commit = '●',
-        -- merge_commit_end = '●',
-      },
-      format = {
-        timestamp = "%Y-%m-%d %H:%M:%S",
-        fields = { "hash", "timestamp", "author", "branch_name", "tag" },
-      },
-      hooks = {
-        -- Check diff of a commit
-        on_select_commit = function(commit)
-          vim.cmd("DiffviewOpen " .. commit.hash .. "^!")
-        end,
-        -- Check diff from commit a -> commit b
-        on_select_range_commit = function(from, to)
-          vim.cmd("DiffviewOpen " .. from.hash .. "~1.." .. to.hash)
-        end,
-      },
-    },
-    config = function(_, opts)
-      require("gitgraph").setup(opts)
-      vim.api.nvim_create_user_command("GitGraph", function()
-        vim.cmd("tabnew")
-        require("gitgraph").draw({}, { all = true, max_count = 5000 })
-      end, {})
-    end
   },
 }
