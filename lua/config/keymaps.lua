8eadeccc677a151c1d13f6d1a4e870642f4921b2
gpncarl <gpncarl@gmail.com>
2024-10-19 14:19:45 +0800

    feat: restore the default map of CTRL-L

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,10 +1,14 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.keymap.set("t", "<c-w>", "<c-\\><c-n><c-w>")
 vim.keymap.del({"n","x"}, "j")
 vim.keymap.del({"n","x"}, "k")
-vim.keymap.del("n", "<c-h>")
-vim.keymap.del("n", "<c-j>")
-vim.keymap.del("n", "<c-k>")
-vim.keymap.del("n", "<c-l>")
+vim.keymap.del("n", "<C-H>")
+vim.keymap.del("n", "<C-J>")
+vim.keymap.del("n", "<C-K>")
+vim.keymap.del("n", "<C-L>")
+
+vim.keymap.set("t", "<C-W>", "<C-\\><C-N><C-W>")
+vim.keymap.set("n", "<C-L>", "<Cmd>nohlsearch<Bar>diffupdate<Bar>normal! <C-L><CR>", {
+  desc = ":help CTRL-L-default",
+})
