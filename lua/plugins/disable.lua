08e493fe6eab5dcdfdea2e4fcb11e7081857e35c
gpncarl <gpncarl@gmail.com>
2024-10-19 14:15:59 +0800

    feat: enable noice and notify etc.

--- lua/plugins/disable.lua
+++ lua/plugins/disable.lua
@@ -1,5 +1,5 @@
 return {
   { "MagicDuck/grug-far.nvim", enabled = false },
-  { "rcarriga/nvim-notify", enabled = false },
-  { "folke/noice.nvim", enabled = false },
+  { "rcarriga/nvim-notify", enabled = true },
+  { "folke/noice.nvim", enabled = true },
 }
