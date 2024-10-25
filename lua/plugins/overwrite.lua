7154fdff5b95e0937f816df7504b2e279e6a091d
gpncarl <gpncarl@gmail.com>
2024-10-25 17:43:46 +0800

    fix: change the keymap of incremental selection

--- lua/plugins/overwrite.lua
+++ lua/plugins/overwrite.lua
@@ -72,4 +72,18 @@ return {
       },
     },
   },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = false,
+          node_incremental = "<Tab>",
+          scope_incremental = false,
+          node_decremental = "<S-Tab>",
+        },
+      },
+    },
+  }
 }
