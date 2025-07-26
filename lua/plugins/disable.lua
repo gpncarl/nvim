e5e3e939da59f97ae406b2b4ec0ff88cfaa7f372
gpncarl <gpncarl@gmail.com>
2025-07-26 23:08:34 +0800

    feat: disable noice.nvim and nvim-notify

--- lua/plugins/disable.lua
+++ lua/plugins/disable.lua
@@ -1,5 +1,5 @@
 return {
   { "MagicDuck/grug-far.nvim", enabled = false, optional = true },
-  { "rcarriga/nvim-notify", enabled = true, optional = true },
-  { "folke/noice.nvim", enabled = true, optional = true },
+  { "rcarriga/nvim-notify", enabled = false, optional = true },
+  { "folke/noice.nvim", enabled = false, optional = true },
 }
