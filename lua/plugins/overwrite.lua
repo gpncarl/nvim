4ad9dfab1cd0bf2a223c28f41c2b32e686628a6b
gpncarl <gpncarl@gmail.com>
2025-09-04 10:23:42 +0800

    feat: enable and overwrite alpha-nvim

--- lua/plugins/overwrite.lua
+++ lua/plugins/overwrite.lua
@@ -163,6 +163,25 @@ return {
     },
   },
   {
+    "goolord/alpha-nvim",
+    optional = true,
+    config = function()
+      local dashboard = require("alpha.themes.startify")
+      -- close Lazy and re-open when the dashboard is ready
+      if vim.o.filetype == "lazy" then
+        vim.cmd.close()
+        vim.api.nvim_create_autocmd("User", {
+          once = true,
+          pattern = "AlphaReady",
+          callback = function()
+            require("lazy").show()
+          end,
+        })
+      end
+      require("alpha").setup(dashboard.opts)
+    end,
+  },
+  {
     "folke/snacks.nvim",
     optional = true,
     opts = {
@@ -170,6 +189,7 @@ return {
         enabled = false,
       },
       dashboard = {
+        enabled = false,
         preset = {
           header = [[
 ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗
