3818c932fe5e07d1ded7ec6c90c4ceba72959648
gpncarl <gpncarl@gmail.com>
2025-09-09 16:03:31 +0800

    feat: remove autocmd close_with_q

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -57,39 +57,39 @@ function M.setup()
   })
 
   -- close some filetypes with <q>
-  vim.api.nvim_create_autocmd("FileType", {
-    group = augroup("close_with_q"),
-    pattern = {
-      "PlenaryTestPopup",
-      "help",
-      "lspinfo",
-      "notify",
-      "qf",
-      "gitgraph",
-      "spectre_panel",
-      "startuptime",
-      "tsplayground",
-      "neotest-output",
-      "checkhealth",
-      "neotest-summary",
-      "neotest-output-panel",
-    },
-    callback = function(event)
-      vim.bo[event.buf].buflisted = false
-      vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
-    end,
-  })
+  -- vim.api.nvim_create_autocmd("FileType", {
+  --   group = augroup("close_with_q"),
+  --   pattern = {
+  --     "PlenaryTestPopup",
+  --     "help",
+  --     "lspinfo",
+  --     "notify",
+  --     "qf",
+  --     "gitgraph",
+  --     "spectre_panel",
+  --     "startuptime",
+  --     "tsplayground",
+  --     "neotest-output",
+  --     "checkhealth",
+  --     "neotest-summary",
+  --     "neotest-output-panel",
+  --   },
+  --   callback = function(event)
+  --     vim.bo[event.buf].buflisted = false
+  --     vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+  --   end,
+  -- })
 
-  vim.api.nvim_create_autocmd("FileType", {
-    group = augroup("tabclose_with_q"),
-    pattern = {
-      "DiffviewFiles"
-    },
-    callback = function(event)
-      vim.bo[event.buf].buflisted = false
-      vim.keymap.set("n", "q", "<cmd>tabclose<cr>", { buffer = event.buf, silent = true })
-    end,
-  })
+  -- vim.api.nvim_create_autocmd("FileType", {
+  --   group = augroup("tabclose_with_q"),
+  --   pattern = {
+  --     "DiffviewFiles"
+  --   },
+  --   callback = function(event)
+  --     vim.bo[event.buf].buflisted = false
+  --     vim.keymap.set("n", "q", "<cmd>tabclose<cr>", { buffer = event.buf, silent = true })
+  --   end,
+  -- })
 
   -- make it easier to close man-files when opened inline
   vim.api.nvim_create_autocmd("FileType", {
