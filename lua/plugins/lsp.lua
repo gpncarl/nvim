9d117e4b9145d6aecfa891fd7decb7378e86279d
gpncarl <gpncarl@gmail.com>
2025-09-09 15:49:32 +0800

    feat: refine lsp configure with new api

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,81 +1,7 @@
 local on_attach = function(client, bufnr)
   vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = bufnr, desc = "goto define" })
   vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = bufnr, desc = "goto declaration" })
-  vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { buffer = bufnr, desc = "goto implementation" })
-  vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = bufnr, desc = "code hover" })
-  vim.keymap.set('n', "<c-k>", vim.lsp.buf.signature_help, { buffer = bufnr, desc = "code signature" })
-
-  vim.keymap.set("n", "grd", function() require("telescope.builtin").lsp_definitions() end,
-    { buffer = bufnr, desc = "goto define" })
-  vim.keymap.set("n", "grr", function() require("telescope.builtin").lsp_references() end,
-    { buffer = bufnr, desc = "goto references" })
-  vim.keymap.set("n", "grD", vim.lsp.buf.declaration, { buffer = bufnr, desc = "goto declaration" })
-  vim.keymap.set("n", "gri", function() require("telescope.builtin").lsp_implementations() end,
-    { buffer = bufnr, desc = "goto implementation" })
-  vim.keymap.set({ "n", "v" }, "grf", vim.lsp.buf.format, { buffer = bufnr, desc = "format" })
-  vim.keymap.set("n", "grk", vim.lsp.buf.hover, { buffer = bufnr, desc = "code hover" })
-  vim.keymap.set('n', "grK", vim.lsp.buf.signature_help, { buffer = bufnr, desc = "code signature" })
-
-  if client.supports_method("textDocument/inlayHint") then
-    vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
-  end
-
-  if client.supports_method("textDocument/codeLens") then
-    local refresh = function() vim.lsp.codelens.refresh({ bufnr = bufnr }) end
-    refresh()
-    vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "InsertLeave" }, {
-      buffer = bufnr,
-      callback = refresh,
-    })
-  end
-end
-
-local get_default_opts = function()
-  return {
-    inlay_hints = { enabled = true, },
-    codelens = { enabled = true, },
-    document_highlight = { enabled = true, },
-    format = {
-      formatting_options = nil,
-      timeout_ms = nil,
-    },
-    capabilities = require("cmp_nvim_lsp").default_capabilities(),
-    on_attach = on_attach,
-  }
-end
-
-local default_lsp_handler = function(server_name)
-  require("lspconfig")[server_name].setup(get_default_opts())
-end
-
-local get_lsp_handler = function()
-  local default_opts = get_default_opts()
-  local lspconfig = require("lspconfig")
-  return {
-    clangd = function()
-      lspconfig.clangd.setup(vim.tbl_extend("force", default_opts, {
-        on_attach = function(client, bufnr)
-          on_attach(client, bufnr)
-          require("clangd_extensions").setup()
-          vim.keymap.set("n", "grh", "<cmd>ClangdSwitchSourceHeader<cr>",
-            { buffer = bufnr, desc = "switch header" })
-          vim.keymap.set("n", "<leader>sh", "<cmd>ClangdSwitchSourceHeader<cr>",
-            { buffer = bufnr, desc = "switch header" })
-        end,
-        cmd = { "clangd", "--offset-encoding=utf-16" },
-      }))
-    end,
-    lua_ls = function()
-      lspconfig.lua_ls.setup(vim.tbl_extend("force", default_opts, {
-        settings = {
-          Lua = {
-            diagnostics = { globals = { "vim" } },
-            codeLens = { enable = true, }
-          }
-        }
-      }))
-    end
-  }
+  vim.keymap.set({ "n", "v" }, "<leader>cf", vim.lsp.buf.format, { buffer = bufnr, desc = "format" })
 end
 
 return {
@@ -120,56 +46,28 @@ return {
     "williamboman/mason.nvim",
     cmd = { "Mason", "MasonUpdate", "MasonLog", "MasonInstall", "MasonUninstall", "MasonUninstallAll" },
     build = ":MasonUpdate",
-    opts = {}
+    opts = {
+      PATH = "append",
+    }
   },
   {
-    "williamboman/mason-lspconfig.nvim",
-    event = { "BufReadPre", "BufNewFile" },
+    "neovim/nvim-lspconfig",
     dependencies = {
       { "williamboman/mason.nvim" },
-      {
-        "neovim/nvim-lspconfig",
-        dependencies = {
-          { "p00f/clangd_extensions.nvim" },
-          { "folke/neoconf.nvim",         opts = {} },
-        },
-      },
     },
     config = function()
-      local manual_installed = { "clangd" }
-      local ensure_installed = { "lua_ls" }
-      local handlers = get_lsp_handler()
-      for _, server_name in ipairs(manual_installed) do
-        ensure_installed[server_name] = nil
-        if handlers[server_name] ~= nil then
-          handlers[server_name]()
-        else
-          default_lsp_handler(server_name)
+      vim.api.nvim_create_autocmd('LspAttach', {
+        callback = function(ev)
+          local client = vim.lsp.get_client_by_id(ev.data.client_id)
+          for bufnr, _ in pairs(client.attached_buffers) do
+            on_attach(client, bufnr)
+          end
         end
-      end
-
-      handlers[1] = default_lsp_handler
-      require("mason-lspconfig").setup({
-        ensure_installed = ensure_installed,
-        handlers = handlers,
       })
-    end
-  },
-  {
-    "jay-babu/mason-null-ls.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-    dependencies = {
-      { "williamboman/mason.nvim" },
-      { "nvimtools/none-ls.nvim", opts = {} },
-    },
-    opts = function()
-      local null_ls = require("null-ls")
-      return {
-        ensure_installed = {},
-        handlers = {
-          nil, -- use default handler
-        },
-      }
+      vim.lsp.enable({
+        "lua_ls",
+        "clangd",
+      })
     end
   },
   {
