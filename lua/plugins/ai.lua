b5379aac95015e11af5d0a5f964657a90e843e34
gpncarl <gpncarl@gmail.com>
2025-09-09 15:47:36 +0800

    feat: replace nvim-cmp with blink.cmp

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -1,7 +1,7 @@
 return {
   {
     "zbirenbaum/copilot.lua",
-    lazy = true,
+    event = { "InsertEnter" },
     opts = {
       suggestion = { enabled = false },
       panel = { enabled = false },
--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -53,7 +53,6 @@ local function cmp_config()
     sources = {
       { name = "nvim_lsp" },
       { name = "buffer" },
-      { name = "copilot" },
       { name = "path" },
       { name = "snippets" },
       { name = "cmp_tabnine" },
@@ -74,7 +73,6 @@ local function cmp_config()
           snippets = "[S]",
           nvim_lsp = "[L]",
           cmp_tabnine = "[T]",
-          copilot = "[C]",
           supermaven = "[M]",
         })[entry.source.name]
         return vim_item
@@ -104,6 +102,7 @@ return {
   },
   {
     "hrsh7th/nvim-cmp",
+    cond = false,
     event = { "InsertEnter", "CmdlineEnter" },
     dependencies = {
       { "hrsh7th/cmp-path" },
@@ -122,13 +121,66 @@ return {
         "tzachar/cmp-tabnine",
         build = "bash install.sh",
       },
-      {
-        "zbirenbaum/copilot-cmp",
-        cond = config.copilot,
-        dependencies = { "zbirenbaum/copilot.lua" },
-        opts = {}
-      },
     },
     config = cmp_config
   },
+  {
+    "saghen/blink.cmp",
+    dependencies = { "rafamadriz/friendly-snippets" },
+    opts = {
+      -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+      -- 'super-tab' for mappings similar to vscode (tab to accept)
+      -- 'enter' for enter to accept
+      -- 'none' for no mappings
+      --
+      -- All presets have the following mappings:
+      -- C-space: Open menu or open docs if already open
+      -- C-n/C-p or Up/Down: Select next/previous item
+      -- C-e: Hide menu
+      -- C-k: Toggle signature help (if signature.enabled = true)
+      --
+      -- See :h blink-cmp-config-keymap for defining your own keymap
+      keymap = { preset = "enter" },
+
+      appearance = {
+        -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+        -- Adjusts spacing to ensure icons are aligned
+        nerd_font_variant = "mono"
+      },
+
+      -- (Default) Only show the documentation popup when manually triggered
+      completion = { documentation = { auto_show = false } },
+
+      -- Default list of enabled providers defined so that you can extend it
+      -- elsewhere in your config, without redefining it, due to `opts_extend`
+      sources = {
+        default = { "lsp", "path", "snippets", "buffer" },
+      },
+
+      -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+      -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+      -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+      --
+      -- See the fuzzy documentation for more information
+      fuzzy = { implementation = "prefer_rust_with_warning" }
+    },
+    opts_extend = { "sources.default" }
+  },
+  config = function(_, opts)
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "BlinkCmpMenuOpen",
+      callback = function()
+        vim.b.copilot_suggestion_hidden = true
+      end,
+    })
+
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "BlinkCmpMenuClose",
+      callback = function()
+        vim.b.copilot_suggestion_hidden = false
+      end,
+    })
+
+    require("blink.cmp").setup(opts)
+  end
 }
