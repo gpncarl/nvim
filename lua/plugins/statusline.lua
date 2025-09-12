209e6f827e4f197c61ffa3a8d3501929e89f4fc1
gpncarl <gpncarl@gmail.com>
2025-09-12 14:57:36 +0800

    fix(lualine): lazy load trouble component

--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -2,84 +2,83 @@ return {
   {
     "nvim-lualine/lualine.nvim",
     event = "ColorScheme",
-    opts = function()
-      local trouble = require("trouble")
-      local symbols = trouble.statusline({
-        mode = "lsp_document_symbols",
-        groups = {},
-        title = false,
-        filter = { range = true },
-        format = " {kind_icon}{symbol.name:Normal}",
-        hl_group = "Normal",
-      })
-
-      return {
-        options = {
-          icons_enabled = true,
-          theme = "auto",
-          disabled_filetypes = {
-            statusline = {
-              "alpha",
-            },
-            winbar = {
-              "alpha",
-              "qf",
-              "toggleterm",
-              "gitcommit",
-              "neo-tree",
-              "fugitive",
-              "help",
-              "git",
-            }
+    opts = {
+      options = {
+        icons_enabled = true,
+        theme = "auto",
+        disabled_filetypes = {
+          statusline = {
+            "alpha",
           },
-          ignore_focus = {},
-          always_divide_middle = true,
-          always_show_tabline = true,
-          refresh = { statusline = 1000, tabline = 1000, winbar = 1000 },
-          globalstatus = true,
-          component_separators = '',
-          -- section_separators = { left = '', right = ' ' },
-          -- section_separators = { left = '', right = '' },
+          winbar = {
+            "alpha",
+            "qf",
+            "toggleterm",
+            "gitcommit",
+            "neo-tree",
+            "fugitive",
+            "help",
+            "git",
+          }
         },
-        sections = {
-          lualine_a = { "mode" },
-          lualine_b = { "branch", "diff", "diagnostics" },
-          lualine_c = { "filename" },
-          lualine_x = { "%S", "searchcount", "encoding", "fileformat", "filetype" },
-          lualine_y = { "lsp_status", "progress" },
-          lualine_z = { "location" },
-        },
-        inactive_sections = {
-          lualine_a = {},
-          lualine_b = {},
-          lualine_c = { "filename" },
-          lualine_x = { "location" },
-          lualine_y = {},
-          lualine_z = {}
-        },
-        winbar = {
-          lualine_c = {
-            {
-              function() return "Symbols" end,
-              draw_empty = true,
-              color = "WinBar",
-              padding = 0,
-              cond = function ()
-                return vim.lsp.buf_is_attached(0)
-              end,
-            },
-            {
-              symbols.get,
-              cond = function ()
-                return vim.lsp.buf_is_attached(0) and symbols.has()
-              end,
-              color = "WinBar",
-            }
+        ignore_focus = {},
+        always_divide_middle = true,
+        always_show_tabline = true,
+        refresh = { statusline = 1000, tabline = 1000, winbar = 1000 },
+        globalstatus = true,
+        component_separators = '',
+        -- section_separators = { left = '', right = ' ' },
+        -- section_separators = { left = '', right = '' },
+      },
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch", "diff", "diagnostics" },
+        lualine_c = { "filename" },
+        lualine_x = { "%S", "searchcount", "encoding", "fileformat", "filetype" },
+        lualine_y = { "lsp_status", "progress" },
+        lualine_z = { "location" },
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { "filename" },
+        lualine_x = { "location" },
+        lualine_y = {},
+        lualine_z = {}
+      },
+      winbar = {
+        lualine_c = {
+          {
+            function() return "Symbols" end,
+            draw_empty = true,
+            color = "WinBar",
+            padding = 0,
+            cond = function()
+              return vim.lsp.buf_is_attached(0)
+            end,
           },
+          {
+            function()
+              local trouble = require("trouble")
+              local symbols = trouble.statusline({
+                mode = "lsp_document_symbols",
+                groups = {},
+                title = false,
+                filter = { range = true },
+                format = " {kind_icon}{symbol.name:WinBar}",
+                hl_group = "WinBar",
+              })
+              return symbols.get() or ""
+            end,
+            cond = function()
+              return vim.lsp.buf_is_attached(0)
+            end,
+            color = "WinBar",
+          }
         },
-        extensions = { "quickfix", "fugitive", "lazy", "mason", "neo-tree", "oil", "trouble" }
-      }
-    end
+      },
+      extensions = { "quickfix", "fugitive", "lazy", "mason", "neo-tree", "oil", "trouble" }
+    }
   },
   {
     "akinsho/bufferline.nvim",
@@ -93,6 +92,7 @@ return {
         numbers = "buffer_id",
         separator_style = "slant",
         sort_by = "id",
+        always_show_bufferline = false,
       }
     }
   },
