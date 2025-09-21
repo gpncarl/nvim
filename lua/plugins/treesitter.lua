d30656f608f3fc5ca7cb519886c2b866a245cc31
gpncarl <gpncarl@gmail.com>
2025-09-21 15:36:49 +0800

    feat: switch to main branch of nvim-treesitter/textobjects

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,53 +1,81 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    main = "nvim-treesitter.configs",
+    branch = "main",
     event = { "BufReadPost", "BufNewFile" },
-    cmd = { "TSUpdateSync", "TSUpdate", "TSUninstall" },
+    cmd = { "TSInstall", "TSUninstall", "TSUpdate" },
     build = ":TSUpdate",
-    opts = {
-      ensure_installed = { "bash", "lua", "c", "cpp", "python", "vim", "comment", "vimdoc" },
-      indent = { enable = true },
-      highlight = { enable = true, use_languagetree = true },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          node_incremental = "<TAB>",
-          node_decremental = "<S-TAB>",
-        },
-      },
-    },
+    config = function()
+      local ensure_installed = { "bash", "lua", "c", "cpp", "python", "vim", "comment", "vimdoc" }
+      require('nvim-treesitter').install(ensure_installed)
+      vim.api.nvim_create_autocmd("FileType", {
+        group = require("utils").augroup("treesitter_enable"),
+        desc = "auto enable treesitter",
+        callback = function()
+          local ok = pcall(function() vim.treesitter.start() end)
+          if ok then
+            vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+          end
+        end,
+      })
+    end,
   },
   {
     "nvim-treesitter/nvim-treesitter-textobjects",
-    event = "ModeChanged *:no",
-    keys = { "[", "]" },
+    branch = "main",
     dependencies = { "nvim-treesitter/nvim-treesitter" },
-    opts = {
-      select = {
-        enable = true,
-        keymaps = {
-          ["af"] = "@function.outer",
-          ["if"] = "@function.inner",
-          ["ac"] = "@class.outer",
-          ["ic"] = "@class.inner"
+    keys = function()
+      local key_spec = {}
+      local select = {
+        ["af"] = "@function.outer",
+        ["if"] = "@function.inner",
+        ["ac"] = "@class.outer",
+        ["ic"] = "@class.inner"
+      }
+      for key, query in pairs(select) do
+        key_spec[#key_spec + 1] = {
+          key,
+          function()
+            require("nvim-treesitter-textobjects.select").select_textobject(query, "textobjects")
+          end,
+          mode = { "o", "x" },
+          desc = "TS textobject select " .. query,
+          silent = true,
         }
-      },
-      move = {
-        enable = true,
-        set_jumps = true,
+      end
+
+      local move = {
         goto_next_start = { ["]m"] = "@function.outer", ["]]"] = "@class.outer" },
         goto_next_end = { ["]M"] = "@function.outer", ["]["] = "@class.outer" },
         goto_previous_start = { ["[m"] = "@function.outer", ["[["] = "@class.outer" },
         goto_previous_end = { ["[M"] = "@function.outer", ["[]"] = "@class.outer" },
         goto_next = { ["]e"] = "@conditional.outer", },
         goto_previous = { ["[e"] = "@conditional.outer", },
-      },
-    },
-    config = function(_, opts)
-      require("nvim-treesitter.configs").setup({ textobjects = opts })
+      }
 
-      local ts_repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
+      for method, keymaps in pairs(move) do
+        for key, query in pairs(keymaps) do
+          key_spec[#key_spec + 1] = {
+            key,
+            function()
+              if vim.wo.diff and key:find("[cC]") then
+                return vim.cmd("normal! " .. key)
+              end
+              require("nvim-treesitter-textobjects.move")[method](query, "textobjects")
+            end,
+            mode = { "n", "x", "o" },
+            desc = "TS textobject move " .. method .. " " .. query,
+            silent = true,
+          }
+        end
+      end
+
+      return key_spec
+    end,
+    config = function()
+      require("nvim-treesitter-textobjects").setup()
+      local ts_repeat_move = require "nvim-treesitter-textobjects.repeatable_move"
+
       vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move)
       vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_opposite)
 
@@ -55,6 +83,6 @@ return {
       vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F_expr, { expr = true })
       vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t_expr, { expr = true })
       vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T_expr, { expr = true })
-    end
+    end,
   },
 }
