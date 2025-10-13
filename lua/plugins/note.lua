7e94ca9d09c43f4c357007555da7e517b8c139a9
gpncarl <gpncarl@gmail.com>
2025-10-13 17:52:30 +0800

    feat(orgmode): reconfigure

--- lua/plugins/note.lua
+++ lua/plugins/note.lua
@@ -1,8 +1,31 @@
 return {
   {
     "nvim-orgmode/orgmode",
+    dependencies = { "tpope/vim-repeat" },
+    cmd = { "Org" },
     ft = { "org" },
-    opts = {},
+    keys = {
+      {
+        "<leader>oa",
+        function()
+          require("orgmode").action("agenda.prompt")
+        end,
+        desc = "org agenda",
+      },
+      {
+        "<leader>oc",
+        function()
+          require("orgmode").action("capture.prompt")
+        end,
+        desc = "org capture",
+      },
+    },
+    opts = {
+      org_agenda_files = "~/org/*",
+      org_default_notes_file = "~/org/notes.org",
+      org_startup_indented = true,
+      org_startup_folded = "inherit",
+    },
   },
   {
     "OXY2DEV/markview.nvim",
