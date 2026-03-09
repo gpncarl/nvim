021b286c1c05303ea82035c0bc84511583740ee3
gpncarl <gpncarl@gmail.com>
2026-03-09 09:43:54 +0800

    style: use <cmd> instead of <Cmd>

--- lua/misc.lua
+++ lua/misc.lua
@@ -25,8 +25,8 @@ function M.setup()
 
   vim.keymap.set("t", "<c-w>", "<c-\\><c-n><c-w>")
   vim.keymap.set("t", "<esc><esc>", "<c-\\><c-n>")
-  vim.keymap.set("n", "[p", "<Cmd>exe 'put! ' . v:register<CR>", { desc = "Paste Above" })
-  vim.keymap.set("n", "]p", "<Cmd>exe 'put '  . v:register<CR>", { desc = "Paste Below" })
+  vim.keymap.set("n", "[p", "<cmd>exe 'put! ' . v:register<cr>", { desc = "Paste Above" })
+  vim.keymap.set("n", "]p", "<cmd>exe 'put '  . v:register<cr>", { desc = "Paste Below" })
 
   vim.g.mapleader = " "
   vim.g.maplocalleader = " "
