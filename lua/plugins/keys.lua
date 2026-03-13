002e4656c85e4b745e7f53d81cd56e8bc5eb492b
gpncarl <gpncarl@gmail.com>
2026-03-14 00:53:12 +0800

    fix: remove treesitter-textobject from mini.ai

--- lua/plugins/keys.lua
+++ lua/plugins/keys.lua
@@ -35,21 +35,7 @@ return {
   {
     "nvim-mini/mini.ai",
     event = "ModeChanged *:no",
-    dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
-    opts = function()
-      local ai = require("mini.ai")
-      return {
-        n_lines = 500,
-        custom_textobjects = {
-          o = ai.gen_spec.treesitter({
-            a = { "@block.outer", "@conditional.outer", "@loop.outer" },
-            i = { "@block.inner", "@conditional.inner", "@loop.inner" },
-          }, {}),
-          f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }, {}),
-          c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }, {}),
-        },
-      }
-    end,
+    opts = {},
   },
   {
     "folke/which-key.nvim",
