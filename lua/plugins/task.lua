fdd48e0295e4587fb29ba339a21c7ea1cf10e31f
gpncarl <gpncarl@gmail.com>
2026-04-08 10:50:38 +0800

    feat: add overseer.nvim for task management

---
+++ lua/plugins/task.lua
@@ -0,0 +1,12 @@
+return {
+  "stevearc/overseer.nvim",
+  cmd = {
+    "OverseerRun",
+    "OverseerToggle",
+    "OverseerShell",
+    "OverseerTaskAction",
+    "OverseerClose",
+    "OverseerOpen",
+  },
+  opts = {},
+}
