3cdfcb2271034cfebfefeae8794f6174984d8025
gpncarl <gpncarl@gmail.com>
2026-04-09 00:04:46 +0800

    feat: add jumph plugin and remove leap

--- lua/plugins/motion.lua
+++ lua/plugins/motion.lua
@@ -19,38 +19,24 @@ return {
     end
   },
   {
-    "https://codeberg.org/andyg/leap.nvim",
+    "local/jumph",
+    dev = true,
     keys = {
-      { "s", "<Plug>(leap-forward)",  mode = { "n", "x", "o" }, desc = "Leap forward", },
-      { "S", "<Plug>(leap-backward)", mode = { "n", "x", "o" }, desc = "Leap backward", },
       {
         "<cr>",
-        function()
-          vim.v.hlsearch = false
-          require("leap").leap({
-            pattern = vim.fn.getreg("/"),
-            backward = vim.v.searchforward == 0,
-            opts = { vim_opts = { ["go.smartcase"] = true, }, }
-          })
-        end,
+        function() require("jumph").jump(vim.fn.getreg("/")) end,
         mode = { "n", "x", "o" },
-        desc = "Leap last search",
+        desc = "Jumph: jump to last search",
       },
     },
-    dependencies = { "tpope/vim-repeat" },
     init = function()
       local au = function(event, pattern, callback, desc)
-        vim.api.nvim_create_autocmd(event, { pattern = pattern, group = require("utils").augroup("leap_last_search"), callback = callback, desc = desc })
+        vim.api.nvim_create_autocmd(event, { pattern = pattern, group = require("utils").augroup("jumph"), callback = callback, desc = desc })
       end
-      local revert_cr = function() vim.keymap.set('n', '<cr>', '<cr>', { buffer = true }) end
+      local revert_cr = function() vim.keymap.set('', '<cr>', '<cr>', { buffer = true }) end
       au('FileType', 'qf', revert_cr, 'Revert <cr>')
       au('CmdwinEnter', '*', revert_cr, 'Revert <cr>')
     end,
-    config = function()
-      local opts = require("leap").opts
-      opts.keys.next_target = { ";" }
-      opts.keys.prev_target = { "," }
-      opts.preview = false
-    end,
+    opts = {},
   }
 }
---
+++ plugins/jumph/lua/jumph.lua
@@ -0,0 +1,73 @@
+local NS = vim.api.nvim_create_namespace('jumph')
+local LABELS = vim.split('fjdkslgha;rueiwotyqpvbcnxmzFJDKSLGHARUEIWOTYQPVBCNXMZ', '')
+
+local M = {}
+
+function M.jump(pattern)
+  if not pattern or #pattern == 0 then
+    return
+  end
+
+  vim.v.hlsearch = false
+  local bufnr = vim.api.nvim_get_current_buf()
+  local line_idx_start, line_idx_end = vim.fn.line('w0'), vim.fn.line('w$')
+  vim.api.nvim_buf_clear_namespace(bufnr, NS, 0, -1)
+
+  local char_idx = 1
+  local extmarks = {}
+  local lines = vim.api.nvim_buf_get_lines(bufnr, line_idx_start - 1, line_idx_end, false)
+  local is_case_sensitive = pattern ~= string.lower(pattern)
+
+  for lines_i, line_text in ipairs(lines) do
+    local search_line = is_case_sensitive and line_text or string.lower(line_text)
+    local search_pattern = is_case_sensitive and pattern or string.lower(pattern)
+    local line_idx = lines_i + line_idx_start - 1
+
+    if vim.fn.foldclosed(line_idx) == -1 then
+      local col = 1
+      while true do
+        local start, stop = search_line:find(search_pattern, col, true)
+        if not start then
+          break
+        end
+        col = stop + 1
+
+        if char_idx <= #LABELS then
+          local overlay_char = LABELS[char_idx]
+          local linenr = line_idx_start + lines_i - 2
+          local id = vim.api.nvim_buf_set_extmark(bufnr, NS, linenr, start - 1, {
+            virt_text = { { overlay_char, 'CurSearch' } },
+            virt_text_pos = 'overlay',
+            hl_mode = 'replace',
+          })
+          extmarks[overlay_char] = { line = linenr, col = start - 1, id = id }
+          char_idx = char_idx + 1
+        end
+      end
+    end
+  end
+
+  vim.schedule(function()
+    if char_idx == 2 then
+      local pos = extmarks[LABELS[1]]
+      vim.cmd("normal! m'")
+      vim.api.nvim_win_set_cursor(0, { pos.line + 1, pos.col })
+    elseif char_idx > 2 then
+      local next_char = vim.fn.nr2char(vim.fn.getchar())
+      if extmarks[next_char] then
+        local pos = extmarks[next_char]
+        vim.cmd("normal! m'")
+        vim.api.nvim_win_set_cursor(0, { pos.line + 1, pos.col })
+      end
+    end
+    vim.api.nvim_buf_clear_namespace(0, NS, 0, -1)
+  end)
+end
+
+function M.setup(opts)
+  if opts and opts.labels then
+    LABELS = vim.split(opts.labels, '')
+  end
+end
+
+return M
