309c40d576a93e14b788f1fb3ceafb90beb2dc2b
gpncarl <gpncarl@gmail.com>
2026-07-03 02:31:09 +0800

    style: format

--- lua/plugins/filebrowser.lua
+++ lua/plugins/filebrowser.lua
@@ -23,7 +23,8 @@ return {
     "nvim-neo-tree/neo-tree.nvim",
     keys = {
       { "<leader>nt", "<cmd>Neotree toggle<cr>",         desc = "Neotree toggle" },
-      { "<leader>nb", "<cmd>Neotree buffers toggle<cr>", desc = "Neotree buffers toggle" } },
+      { "<leader>nb", "<cmd>Neotree buffers toggle<cr>", desc = "Neotree buffers toggle" }
+    },
     opts = {
       buffers = { follow_current_file = { enabled = true }, group_empty_dirs = true, show_unloaded = true },
       filesystem = { hijack_netrw_behavior = "disabled" }
