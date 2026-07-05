0877f0f58ced627d5e334a9f6c31030915df75cb
gpncarl <gpncarl@gmail.com>
2026-07-06 00:06:11 +0800

    feat(picker): unify picker interface

--- lua/plugins/finder.lua
+++ lua/plugins/finder.lua
@@ -1,159 +1,63 @@
 local config = require("config")
-local function telescope_config()
-  local telescope = require("telescope")
-  local actions = require("telescope.actions")
-  local layout = require("telescope.actions.layout")
-  telescope.setup {
-    extensions = {
-      fzf = {
-        fuzzy = true,
-        override_generic_sorter = true,
-        override_file_sorter = true,
-        case_mode = "smart_case"
-      }
-    },
-    defaults = {
-      prompt_prefix = "   ",
-      selection_caret = " ",
-      entry_prefix = " ",
-      sorting_strategy = "ascending",
-      layout_config = {
-        horizontal = {
-          prompt_position = "top",
-          preview_width = 0.55,
-        },
-        width = 0.87,
-        height = 0.80,
-      },
-      mappings = {
-        i = {
-          ["<c-_>"] = layout.toggle_preview,
-          ["<c-/>"] = layout.toggle_preview,
-          ["<c-o>"] = actions.move_selection_better + actions.toggle_selection,
-        },
-        n = {
-          ["q"] = require("telescope.actions").close,
-          ["<c-o>"] = actions.move_selection_better + actions.toggle_selection,
-        },
-      },
-      path_display = { "filename_first" },
-      borderchars = { " ", " ", " ", " ", " ", " ", " ", " " },
-      -- preview = { hide_on_startup = true }
-    }
-  }
-
-  telescope.load_extension("fzf")
-end
 
 return {
   {
     "nvim-telescope/telescope.nvim",
     enabled = (config.finder == "telescope"),
     cmd = { "Telescope" },
-    keys = {
-      { "<leader>fo", "<cmd>Telescope treesitter buffer=0<cr>", desc = "fuzzy outline" },
-      { "<leader>fF", "<cmd>Telescope find_files<cr>",          desc = "fuzzy files" },
-      {
-        "<leader>ff",
-        function()
-          require("telescope.builtin").find_files({ cwd = require("utils").root() })
-        end,
-        desc = "fuzzy files(root)"
-      },
-      { "<leader>fb", "<cmd>Telescope buffers<cr>",   desc = "fuzzy buffers" },
-      { "<leader>sG", "<cmd>Telescope live_grep<cr>", desc = "live fuzzy string" },
-      {
-        "<leader>sg",
-        function()
-          require("telescope.builtin").live_grep({ cwd = require("utils").root() })
-        end,
-        desc = "live fuzzy string(root)"
-      },
-      {
-        "<leader>/",
-        function()
-          require("telescope.builtin").live_grep({ cwd = require("utils").root() })
-        end,
-        desc = "live fuzzy string(root)"
-      },
-      { "<leader>fr", "<cmd>Telescope resume<cr>",               desc = "fuzzy resume" },
-      { "<leader>fa", "<cmd>Telescope builtin<cr>",              desc = "fuzzy all built-in" },
-      { "gd",         "<cmd>Telescope lsp_definitions<cr>",      desc = "Goto Definition" },
-      { "gD",         "<cmd>Telescope lsp_definitions<cr>",      desc = "Goto Declaration" },
-      { "grr",        "<cmd>Telescope lsp_references<cr>",       nowait = true,                  desc = "References" },
-      { "gri",        "<cmd>Telescope lsp_implementations<cr>",  desc = "Goto Implementation" },
-      { "grt",        "<cmd>Telescope lsp_type_definitions<cr>", desc = "Goto T[y]pe Definition" },
-    },
     dependencies = {
       {
         "nvim-telescope/telescope-fzf-native.nvim",
         build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release --fresh && cmake --build build --config Release"
       },
     },
-    config = telescope_config
-  },
-  {
-    "nvim-telescope/telescope-frecency.nvim",
-    enabled = (config.finder == "telescope"),
-    cmd = { "Mru" },
-    keys = { { "<leader>fm", "<cmd>Mru<cr>", desc = "fuzzy files by frecency" } },
-    dependencies = { "nvim-telescope/telescope.nvim" },
     config = function()
-      require("telescope").load_extension("frecency")
-      vim.api.nvim_create_user_command("Mru", function()
-        vim.cmd("Telescope frecency")
-      end, {})
-    end,
+      local telescope = require("telescope")
+      local actions = require("telescope.actions")
+      local layout = require("telescope.actions.layout")
+      telescope.setup {
+        extensions = {
+          fzf = {
+            fuzzy = true,
+            override_generic_sorter = true,
+            override_file_sorter = true,
+            case_mode = "smart_case"
+          }
+        },
+        defaults = {
+          prompt_prefix = "   ",
+          selection_caret = " ",
+          entry_prefix = " ",
+          sorting_strategy = "ascending",
+          layout_config = {
+            horizontal = {
+              prompt_position = "top",
+            },
+          },
+          mappings = {
+            i = {
+              ["<c-_>"] = layout.toggle_preview,
+              ["<c-/>"] = layout.toggle_preview,
+              ["<c-o>"] = actions.move_selection_better + actions.toggle_selection,
+            },
+            n = {
+              ["q"] = require("telescope.actions").close,
+              ["<c-o>"] = actions.move_selection_better + actions.toggle_selection,
+            },
+          },
+          path_display = { "filename_first" },
+          -- borderchars = { " ", " ", " ", " ", " ", " ", " ", " " },
+          -- preview = { hide_on_startup = true }
+        }
+      }
+
+      telescope.load_extension("fzf")
+    end
   },
   {
     "ibhagwan/fzf-lua",
-    enabled = (config.finder == "fzf"),
+    enabled = (config.finder == "fzf_lua"),
     cmd = { "FzfLua" },
-    keys = {
-      { "<leader>fo", "<cmd>FzfLua treesitter<cr>",           desc = "fuzzy treesitter" },
-      {
-        "<leader>ff",
-        function()
-          require("fzf-lua").files({ cwd = require("utils").root() })
-        end,
-        desc = "fuzzy files(root)"
-      },
-      { "<leader>fF", "<cmd>FzfLua files<cr>",                desc = "fuzzy files(cwd)" },
-      { "<leader>fr", "<cmd>FzfLua oldfiles<cr>",             desc = "fuzzy oldfiles" },
-      { "<leader>fb", "<cmd>FzfLua buffers<cr>",              desc = "fuzzy buffers" },
-      { "<leader>fa", "<cmd>FzfLua builtin<cr>",              desc = "fuzzy all built-in" },
-      { "<leader>/",  "<cmd>FzfLua live_grep<cr>",            desc = "live fuzzy string" },
-      {
-        "<leader>/",
-        function()
-          require("fzf-lua").live_grep({ cwd = require("utils").root() })
-        end,
-        desc = "fuzzy files(root)"
-      },
-      {
-        "<leader>sg",
-        function()
-          require("fzf-lua").live_grep({ cwd = require("utils").root() })
-        end,
-        desc = "fuzzy files(root)"
-      },
-      { "<leader>sG", "<cmd>FzfLua live_grep<cr>",            desc = "live fuzzy string" },
-      { "<leader>ss", "<cmd>FzfLua lsp_document_symbols<cr>", desc = "fuzzy lsp_document_symbols" },
-      { "<leader>sR", "<cmd>FzfLua resume<cr>",               desc = "fuzzy resume" },
-      { "<leader>sw", "<cmd>FzfLua grep_cword<cr>",           desc = "fuzzy grep_cword" },
-      { "<leader>sW", "<cmd>FzfLua grep_cWORD<cr>",           desc = "fuzzy grep_cWORD" },
-      { "gd",         "<cmd>FzfLua lsp_definitions<cr>",      desc = "Goto Definition" },
-      { "gD",         "<cmd>FzfLua lsp_definitions<cr>",      desc = "Goto Declaration" },
-      { "grr",        "<cmd>FzfLua lsp_references<cr>",       nowait = true,                      desc = "References" },
-      { "gri",        "<cmd>FzfLua lsp_implementations<cr>",  desc = "Goto Implementation" },
-      { "grt",        "<cmd>FzfLua lsp_type_definitions<cr>", desc = "Goto T[y]pe Definition" },
-    },
-    init = function()
-      vim.ui.select = function(...)
-        require("fzf-lua").register_ui_select()
-        return vim.ui.select(...)
-      end
-    end,
     opts = {
       fzf_opts = {
         ["--cycle"] = true
@@ -171,4 +75,37 @@ return {
       },
     },
   },
+  {
+    "2KAbhishek/pickme.nvim",
+    cmd = { "PickMe" },
+    keys = function()
+      local pickme = require("pickme")
+      return {
+        { "<leader>,",  function() pickme.pick("buffers") end,                                        desc = "Buffers" },
+        { "<leader>/",  function() pickme.pick("live_grep", { cwd = require("utils").root() }) end,   desc = "Grep(root)" },
+        { "<leader>fo", function() pickme.pick("treesitter") end,                                     desc = "Treesitter" },
+        { "<leader>fb", function() pickme.pick("buffers") end,                                        desc = "Buffers" },
+        { "<leader>ff", function() pickme.pick("files", { cwd = require("utils").root() }) end,       desc = "Find Files(root)" },
+        { "<leader>fF", function() pickme.pick("files") end,                                          desc = "Find Files(cwd)" },
+        { "<leader>fr", function() pickme.pick("oldfiles") end,                                       desc = "Recent" },
+        { "<leader>sg", function() pickme.pick("live_grep", { cwd = require("utils").root() }) end,   desc = "Grep(root)" },
+        { "<leader>sG", function() pickme.pick("live_grep") end,                                      desc = "Grep(cwd)" },
+        { "<leader>sR", function() pickme.pick("resume") end,                                         desc = "Resume" },
+        { "<leader>sw", function() pickme.pick("grep_string", { cwd = require("utils").root() }) end, desc = "Visual selection or word(root)", mode = { "n", "x" } },
+        { "<leader>sW", function() pickme.pick("grep_string") end,                                    desc = "Visual selection or word(cwd)",  mode = { "n", "x" } },
+        { "gd",         function() pickme.pick("lsp_definitions") end,                                desc = "Goto Definition" },
+        { "gD",         function() pickme.pick("lsp_declarations") end,                               desc = "Goto Declaration" },
+        { "grr",        function() pickme.pick("lsp_references") end,                                 nowait = true,                           desc = "References" },
+        { "gri",        function() pickme.pick("lsp_implementations") end,                            desc = "Goto Implementation" },
+        { "grt",        function() pickme.pick("lsp_type_definitions") end,                           desc = "Goto T[y]pe Definition" },
+        { "<leader>ss", function() pickme.pick("lsp_document_symbols") end,                           desc = "LSP Symbols" },
+        { "<leader>sS", function() pickme.pick("lsp_workspace_symbols") end,                          desc = "LSP Workspace Symbols" },
+      }
+    end,
+    opts = {
+      picker_provider = config.finder,
+      detect_provider = false,
+      add_default_keybindings = false,
+    }
+  }
 }
--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -5,34 +5,8 @@ return {
     priority = 1000,
     lazy = false,
     keys = {
-      { "<leader>fl",      function() Snacks.picker.picker_layouts() end,                             desc = "Layout" },
-      { "<leader>fo",      function() Snacks.picker.treesitter() end,                                 desc = "Treesitter" },
-      { "<leader><enter>", function() Snacks.picker.smart() end,                                      desc = "Smart Find Files" },
-      { "<leader>,",       function() Snacks.picker.buffers() end,                                    desc = "Buffers" },
-      { "<leader>/",       function() Snacks.picker.grep({ cwd = require("utils").root() }) end,      desc = "Grep(root)" },
-      { "<leader>fb",      function() Snacks.picker.buffers() end,                                    desc = "Buffers" },
-      { "<leader>ff",      function() Snacks.picker.files({ cwd = require("utils").root() }) end,     desc = "Find Files(root)" },
-      { "<leader>fF",      function() Snacks.picker.files() end,                                      desc = "Find Files(cwd)" },
-      { "<leader>fr",      function() Snacks.picker.recent() end,                                     desc = "Recent" },
-      { "<leader>fR",      function() Snacks.picker.recent({ filter = { cwd = true } }) end,          desc = "Recent (cwd)" },
-      { "<leader>sg",      function() Snacks.picker.grep({ cwd = require("utils").root() }) end,      desc = "Grep(root)" },
-      { "<leader>sG",      function() Snacks.picker.grep() end,                                       desc = "Grep(cwd)" },
-      { "<leader>sR",      function() Snacks.picker.resume() end,                                     desc = "Resume" },
-      { "<leader>sw",      function() Snacks.picker.grep_word({ cwd = require("utils").root() }) end, desc = "Visual selection or word(root)", mode = { "n", "x" } },
-      { "<leader>sW",      function() Snacks.picker.grep_word() end,                                  desc = "Visual selection or word(cwd)",  mode = { "n", "x" } },
-      { "<leader>sb",      function() Snacks.picker.lines() end,                                      desc = "Buffer Lines" },
-      { "<leader>sB",      function() Snacks.picker.grep_buffers() end,                               desc = "Grep Open Buffers" },
-      { "gd",              function() Snacks.picker.lsp_definitions() end,                            desc = "Goto Definition" },
-      { "gD",              function() Snacks.picker.lsp_declarations() end,                           desc = "Goto Declaration" },
-      { "grr",             function() Snacks.picker.lsp_references() end,                             nowait = true,                           desc = "References" },
-      { "gri",             function() Snacks.picker.lsp_implementations() end,                        desc = "Goto Implementation" },
-      { "grt",             function() Snacks.picker.lsp_type_definitions() end,                       desc = "Goto T[y]pe Definition" },
-      { "<leader>ss",      function() Snacks.picker.lsp_symbols() end,                                desc = "LSP Symbols" },
-      { "<leader>sS",      function() Snacks.picker.lsp_workspace_symbols() end,                      desc = "LSP Workspace Symbols" },
       { "<leader>z",       function() Snacks.zen() end,                                               desc = "Toggle Zen Mode" },
       { "<leader>Z",       function() Snacks.zen.zoom() end,                                          desc = "Toggle Zoom" },
-
-
       { "<leader>bd",      function() Snacks.bufdelete() end,                                         desc = "Delete Buffer" },
       { "<c-\\><c-\\>",    function() Snacks.terminal.toggle() end,                                   desc = "Terminal",                       mode = { "n", "t" } },
       { "<c-n>",           function() Snacks.words.jump(1, true) end,                                 desc = "Words next" },
@@ -53,6 +27,14 @@ return {
       input = {},
       picker = {
         enabled = (config.finder == "snacks"),
+        win = {
+          input = {
+            keys = {
+              ["<c-_>"] = { "toggle_preview", mode = { "i", "n" } },
+              ["<c-/>"] = { "toggle_preview", mode = { "i", "n" } },
+            }
+          }
+        }
       },
       quickfile = {},
       statuscolumn = {
