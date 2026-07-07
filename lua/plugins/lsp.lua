820ca054b05771689d335c4a95bdd232a9230403
gpncarl <gpncarl@gmail.com>
2026-07-07 10:27:00 +0800

    fix(lazydev): remove luvit-meta

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -76,11 +76,6 @@ return {
   {
     "folke/lazydev.nvim",
     ft = "lua",
-    dependencies = { "Bilal2453/luvit-meta" },
-    opts = {
-      library = {
-        { path = "luvit-meta/library", words = { "vim%.uv" } },
-      },
-    },
+    opts = {},
   },
 }
