2954e8d3d4c9a67a29d24d80b54483ce794dff79
gpncarl <gpncarl@gmail.com>
2026-07-30 23:07:36 +0800

    feat(autocmd): show lsp progress

--- plugin/autocmd.lua
+++ plugin/autocmd.lua
@@ -142,3 +142,21 @@ vim.api.nvim_create_autocmd("LspAttach", {
     end
   end,
 })
+
+vim.api.nvim_create_autocmd("LspProgress", {
+  group = augroup("lsp_progress"),
+  callback = function(ev)
+    local client = vim.lsp.get_client_by_id(ev.data.client_id)
+    local client_name = client and client.name or "unknown"
+    local value = ev.data.params.value
+    local msg = (value.message or "done") .. string.format("(%s)", client_name)
+    vim.api.nvim_echo({ { msg } }, false, {
+      id = "lsp." .. ev.data.params.token,
+      kind = "progress",
+      source = "vim.lsp",
+      title = value.title,
+      status = value.kind ~= "end" and "running" or "success",
+      percent = value.percentage,
+    })
+  end,
+})
