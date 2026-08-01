40fabd49dbb45a5c43fd07c40978d954e9b3d6e6
gpncarl <gpncarl@gmail.com>
2026-08-02 04:05:19 +0800

    chore: reset rtp path to optimize startup time

--- init.lua
+++ init.lua
@@ -11,6 +11,19 @@ vim.g.user_enable_nvim_dir_plugin = true
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+local lib = vim.fn.fnamemodify(vim.v.progpath, ":p:h:h") .. "/lib"
+lib = vim.uv.fs_stat(lib .. "64") and (lib .. "64") or lib
+lib = lib .. "/nvim"
+local rtp = {
+  vim.fn.stdpath("config"),
+  vim.fn.stdpath("data") .. "/site",
+  vim.env.VIMRUNTIME,
+  lib,
+  vim.fn.stdpath("config") .. "/after",
+}
+vim.opt.rtp = rtp
+vim.opt.packpath = rtp
+
 vim.pack.add({ "https://github.com/zuqini/zpack.nvim" })
 require('zpack').setup()
 vim.cmd.colorscheme(vim.g.user_colorscheme)
