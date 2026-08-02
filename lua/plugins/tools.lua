e53c6854e6037ee4f796755fa9032aa5313bc84e
gpncarl <gpncarl@gmail.com>
2026-08-02 14:06:22 +0800

    fix: fix leetcode.nvim

--- lua/plugins/tools.lua
+++ lua/plugins/tools.lua
@@ -5,24 +5,11 @@ return {
     cmd = "Leet",
     build = ":TSUpdate html",
     dependencies = {
-      "nvim-telescope/telescope.nvim",
       "MunifTanjim/nui.nvim",
       "nvim-treesitter/nvim-treesitter",
     },
     opts = {
       cn = { enabled = true },
-      injector = {
-        ["python3"] = {
-          before = true
-        },
-        ["cpp"] = {
-          before = { "#include <bits/stdc++.h>", "using namespace std;" },
-          after = "int main() {}",
-        },
-        ["java"] = {
-          before = "import java.util.*;",
-        },
-      }
     },
   },
   {
