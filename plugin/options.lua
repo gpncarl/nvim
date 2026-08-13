e776919a24ceeec2957d348e01d38ac49e51e55e
gpncarl <gpncarl@gmail.com>
2026-08-13 11:32:57 +0800

    feat: only support nvim version >=0.12

--- plugin/options.lua
+++ plugin/options.lua
@@ -3,7 +3,7 @@ vim.opt.foldcolumn = "0"
 vim.opt.foldtext = ""
 vim.opt.foldlevelstart = 99
 vim.opt.foldenable = true
-vim.opt.fillchars = { eob = " ", fold = " ", foldopen = "", foldsep = " ", foldclose = "", foldinner = vim.fn.has("nvim-0.12") == 1 and " " or nil }
+vim.opt.fillchars = { eob = " ", fold = " ", foldopen = "", foldsep = " ", foldclose = "", foldinner = " " }
 vim.opt.foldmethod = "expr"
 vim.opt.conceallevel = 2
 vim.opt.cmdheight = 1
