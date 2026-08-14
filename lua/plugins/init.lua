a94143058881d8df09db38c0ae066f78e80c758c
gpncarl <gpncarl@gmail.com>
2026-08-14 10:24:06 +0800

    feat(marks.nvim): remove highlight alias

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -3,11 +3,7 @@ return {
   {
     "chentoast/marks.nvim",
     event = { "BufReadPost", "BufNewFile" },
-    config = function()
-      require("marks").setup()
-      vim.api.nvim_set_hl(0, "MarkSignHL", { link = "CursorLineNr" })
-      vim.api.nvim_set_hl(0, "MarkSignNumHL", { link = "Identifier" })
-    end
+    opts = {},
   },
   {
     "wsdjeg/vim-fetch",
