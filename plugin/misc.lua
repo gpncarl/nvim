adfcaa6f5f0e3857822e13d19386efb08c066b37
gpncarl <gpncarl@gmail.com>
2026-08-28 10:37:45 +0800

    style: quote style

--- plugin/misc.lua
+++ plugin/misc.lua
@@ -17,16 +17,16 @@ MiniMisc.safely("later", function()
     },
     signs = {
       text = {
-        [vim.diagnostic.severity.ERROR] = '',
-        [vim.diagnostic.severity.WARN] = '',
-        [vim.diagnostic.severity.INFO] = '',
-        [vim.diagnostic.severity.HINT] = '',
+        [vim.diagnostic.severity.ERROR] = "",
+        [vim.diagnostic.severity.WARN] = "",
+        [vim.diagnostic.severity.INFO] = "",
+        [vim.diagnostic.severity.HINT] = "",
       },
       numhl = {
-        [vim.diagnostic.severity.ERROR] = 'DiagnosticError',
-        [vim.diagnostic.severity.WARN] = 'DiagnosticWarn',
-        [vim.diagnostic.severity.INFO] = 'DiagnosticInfo',
-        [vim.diagnostic.severity.HINT] = 'DiagnosticHint',
+        [vim.diagnostic.severity.ERROR] = "DiagnosticError",
+        [vim.diagnostic.severity.WARN] = "DiagnosticWarn",
+        [vim.diagnostic.severity.INFO] = "DiagnosticInfo",
+        [vim.diagnostic.severity.HINT] = "DiagnosticHint",
       },
     },
   }
