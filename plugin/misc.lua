c54dd4988755569862ed81988fc98e420ce91f0b
gpncarl <gpncarl@gmail.com>
2026-08-29 21:44:32 +0800

    feat: link CursorLineSign and CursorLineFold to CursorLine

--- plugin/misc.lua
+++ plugin/misc.lua
@@ -1,4 +1,6 @@
 vim.api.nvim_set_hl(0, "CursorLineNr", { link = "CursorLine" })
+vim.api.nvim_set_hl(0, "CursorLineSign", { link = "CursorLine" })
+vim.api.nvim_set_hl(0, "CursorLineFold", { link = "CursorLine" })
 
 MiniMisc.safely("later", function()
   require("vim._core.ui2").enable({
