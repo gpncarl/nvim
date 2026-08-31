755a796e00a3364a435bf42f1302f82264459dff
gpncarl <gpncarl@gmail.com>
2026-08-31 09:18:43 +0800

    fix: add group to autocmd

--- after/ftplugin/nvim-pack.lua
+++ after/ftplugin/nvim-pack.lua
@@ -1,4 +1,6 @@
+local augroup = require("utils").augroup
 vim.api.nvim_create_autocmd("BufDelete", {
+  group = augroup("nvim_pack_buf_delete"),
   once = true,
   buffer = 0,
   callback = vim.schedule_wrap(function()
